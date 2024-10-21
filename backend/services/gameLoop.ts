7a520aeb01539f3cb99e6cce936aa5ea8adfb280
julian <juliankroes2206@gmail.com>
2024-10-21 16:07:29 +0200

    feat: medium now recieves random prompt on device

--- backend/services/gameLoop.ts
+++ backend/services/gameLoop.ts
@@ -1,3 +1,4 @@
+import { Player } from "../models/player.model.ts"
 import { Room } from "../models/room.model.ts"
 import ConnectionService from "./connectionService.ts"
 import GameService from "./gameService.ts"
@@ -22,8 +23,8 @@ export default class GameLoop {
     public async main() {
         await this.explanation(20)
         console.log('done')
-        // voteMedium(20)
-        await this.mediumAnswerPrompt(20)
+        const medium = this.voteMedium()
+        await this.mediumAnswerPrompt(20, medium)
         // spiritsAnswerPrompt(20)
         // displayEncounter(20)
         // mediumInterperatesEncounter(20)
@@ -33,18 +34,27 @@ export default class GameLoop {
 
     async explanation(durationSeconds: number) {
         console.log(`Waiting for ${durationSeconds} seconds`)
-        await new Promise(r => setTimeout(r, 2000));
+        await new Promise(r => setTimeout(r, durationSeconds));
     }
-
-    async mediumAnswerPrompt(durationSeconds: number) {
+    voteMedium(): Player {
+        const playerEntry = this.room.playerList.entries().next()
+        if (!playerEntry.done) {
+            const [, player] = playerEntry.value
+            if (player instanceof Player) {
+                return player
+            }
+        }
+        throw new Error('No player found')
+        
+    }
+    async mediumAnswerPrompt(durationSeconds: number, medium: Player) {
         const randomPrompt: string = await this.promptService.getRandomPrompt()
         const placeholder: string = "fill in the blank"
-
         this.gameService.inputMessage(
             randomPrompt,
             placeholder,
             this.room.roomcode,
-            [this.room.playerList.entries().next().value.name]
+            [medium.name]
         )
-    }
+    }  
 }
